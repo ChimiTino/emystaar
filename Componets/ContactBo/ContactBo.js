076505ab15a85ecf83661c6ad5e25ea3efb010a3
Tinotenda Chibi <tinotendachibi33@gmail.com>
2022-03-24 19:24:43 +0200

    make it better

--- Componets/ContactBo/ContactBo.js
+++ Componets/ContactBo/ContactBo.js
@@ -5,7 +5,7 @@ function ContactBo() {
         <div className='info-containe'>
          <div className='contact-container'>
       <div className='contact-box'>
-      <form  className='contact-info' id='form' action="https://formsubmit.co/tinotendachibi33@gmail.com" method="POST" >
+      <form  className='contact-info' id='form' action="https://formsubmit.co/3d408a5325b9401c27ffaf9a79f32bfb" method="POST" >
         <h2>Contact Us</h2>
         <div className='input-container'>
         <input type="text" name='name' placeholder='Name' required/>
