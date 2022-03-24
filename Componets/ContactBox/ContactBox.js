14fbec323fb2808852e573ae74f01b34e97a8bdc
Tinotenda Chibi <tinotendachibi33@gmail.com>
2022-03-24 19:16:26 +0200

    make it better

--- Componets/ContactBox/ContactBox.js
+++ Componets/ContactBox/ContactBox.js
@@ -8,11 +8,11 @@ function ContactBox({focusRender}) {
       <form  className='contact-info'  action="https://formsubmit.co/3d408a5325b9401c27ffaf9a79f32bfb" method="POST" >
         <h2>Contact Us</h2>
         <div className='input-container'>
-        <input ref={focusRender} type="text" name='name' placeholder='Name' required/>
-        <input type="email" name='email' placeholder='Email' required/>
-        <input type="tel-number" name='number' placeholder='Number' required/>
-        <input type="text" name='service' placeholder="Type of service e.g 'Day Nursing'" required/>
-        <textarea className='message' type='message' name='message' placeholder='Message (Optional)'/>
+        <input ref={focusRender} type="text" name='Name' placeholder='Name' required/>
+        <input type="email" name='Email' placeholder='Email' required/>
+        <input type="tel-number" name='Number' placeholder='Number' required/>
+        <input type="text" name='Service' placeholder="Type of service e.g 'Day Nursing'" required/>
+        <textarea className='message' type='message' name='Message' placeholder='Message (Optional)'/>
         <button type="submit">Send</button>
         </div>
     </form>
